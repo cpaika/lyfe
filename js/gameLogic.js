15a296b852e8ca5fcc8710b2ca632f15a0ce2b37
Chris Paika <paika.christopher@gmail.com>
2014-10-03 15:14:49 -0400

    Adding in alive blocks

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,10 +1,11 @@
 //Lists of constants that should only be modified in the source code
 var UPDATE_INTERVAL = 1000;
+var RECT_SIZE = 10;
 
 
 //lists of global state for testing
-var y = 0;
-var x = 0;
+var aliveX = [];
+var aliveY = [];
 
 function startGame()
 {
@@ -15,12 +16,14 @@ function startGame()
 		document.getElementById("theCanvas").width = window.innerWidth;//TODO this shouldn't be window
 		document.getElementById("theCanvas").height = window.innerHeight;
 	}
+	setInitialState();
 	setInterval("update()", UPDATE_INTERVAL);
 }
 
 function update()
 {
 	drawRects();
+	drawAlive(aliveX, aliveY);
 }
 
 function drawRects()
@@ -29,18 +32,36 @@ function drawRects()
 	var y = 0;
 	var c = document.getElementById("theCanvas");
 	var ctx = c.getContext("2d");
-	ctx.fillStyle = "#FF0000";
-	while(x < c.width)
+	ctx.fillStyle = "#000000";
+	while(x*RECT_SIZE < c.width)
 	{
-		ctx.fillStyle = "#FF0000";
-		while(y < c.height)
+		while(y*RECT_SIZE < c.height)
 		{
-			ctx.fillRect(x,y,x+10,y+10);
-			y = y + 10;
+			ctx.fillRect(x*RECT_SIZE,y*RECT_SIZE,(x*RECT_SIZE)+RECT_SIZE,(y*RECT_SIZE)+RECT_SIZE);
+			y = y + 1;
 		}
 		y = 0;
-		x = x + 10;
+		x = x + 1;
 	}
+}
+
+function drawAlive(aliveX, aliveY)
+{
+	var c = document.getElementById("theCanvas");
+	var ctx = c.getContext("2d");
+	ctx.fillStyle = "#FF0000";
+	for(var i = 0; i < aliveX.length; i++)
+	{
+		var x = aliveX[i]*RECT_SIZE;
+		var y = aliveY[i]*RECT_SIZE;
+		ctx.fillRect(x,y,x+RECT_SIZE,y+RECT_SIZE);
+	}
+}
+
+function setInitialState()
+{
+	aliveX = [1,2,50,60,500];
+	aliveY = [1,5,300,200,500];
 }
 
 function goFullScreen(i)
